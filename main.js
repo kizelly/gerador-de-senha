969b420708de7751ff57eefe77b071ae4a84536d
kizelly <141340994+kizelly@users.noreply.github.com>
2025-06-06 07:31:29 -0300

    klogbg

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
